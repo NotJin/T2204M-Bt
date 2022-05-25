6c3419bdc39a49c208d209ee7f29019730723cc2
Đồng Vũ <Dongvu7173@gmail.com>
2022-05-25 18:39:39 +0700

    b3

--- Sessoion5-Assignment/b3.cpp
+++ Sessoion5-Assignment/b3.cpp
@@ -25,11 +25,8 @@ int main(){
 	int max = arr[0];
 	int min = arr[0];
 	for(int i = 0; i<n; i++){
-		if(arr[i] > max){
-			arr[i] = max;
-		}
-		if(arr[i]< min){
-			arr[i] = min;
+		if(arr[i]>max){
+			max = arr[i];
 		}
 	}
 	printf("[%d, %d] la doan chua cac gia tri trong mang", min, max);
