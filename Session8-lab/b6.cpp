e8928b8bf7d7af21499d750e1513c2b7d8ee8823
Đồng Vũ <Dongvu7173@gmail.com>
2022-06-04 08:58:55 +0700

    bo sung

--- Session8-lab/b6.cpp
+++ Session8-lab/b6.cpp
@@ -1,52 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-int nhapChuoi(char s[][100]){
+int main(){
 	printf("Nhap chuoi s:");
 	for(int i=0; i<10; i++){
 		scanf("%s", s[i]);
 	}
-	printf("Chuoi vua nhap:");
+	char s_max[100];
+	strcpy(s_max, arrs[0]);//s_max = arrs[0]
+	
 	for(int i=0; i<10; i++){
-		printf("%s\n", s[i]);
-	}
-}
-int layKtDau(char s[][100]){
-	char dec;
-	for(int i = 0; i<10; i++){
-		int skt = strlen(s[i]);
-		for(int i=0; i<skt; i++){
-			dec = s[i][100];
-			break;
+		if(strcmp(arrs[i],s_max){
+			strcpy(s_max, arrs[i]);
 		}
 	}
-	return dec;
-}
-int timMin(char s[][100]){
-	int min = 0;
-	int ktd;
-	for(int i=0; i<10; i++){
-		ktd = layKtDau(s);
-		if(ktd<min){
-			min = ktd;
-		}
-	}
-	for(int i=0;i<10;i++){
-        if(ktd==min)
-        {
-            printf("Chuoi dai nhat la %s", s[i]);
-            break;
-        }
-    }
-	//return min;
-}
-//int timChuoiDungTruoc(char s[][100]){
-//	for(int i=0; i<10; i++){
-//		
-//	}
-//}
-int main(){
-	char s[10][100];
-	nhapChuoi(s);
-	timMin(s);
-	//printf("Chuoi dung dau trong day alphabet la: %d", timMin(s));
+	printf("Chuoi dung dau alphabet la: %s", s_max);
 }
